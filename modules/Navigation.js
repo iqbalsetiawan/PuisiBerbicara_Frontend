e20e1672a2ac958b05c9aa51470df245062a885b
Ahmad Reza <hungry.dev39@gmail.com>
2021-07-17 20:36:41 +0700

    create mobile nav popup

--- modules/Navigation.js
+++ modules/Navigation.js
@@ -1,8 +1,36 @@
-import { Flex, Heading, Spacer, IconButton, useMediaQuery, HStack, Link } from '@chakra-ui/react'
-import { HamburgerIcon } from '@chakra-ui/icons'
+import {
+    Flex,
+    Heading,
+    Spacer,
+    IconButton,
+    useMediaQuery,
+    HStack,
+    Link,
+    useDisclosure,
+    Box,
+    VStack,
+ } from '@chakra-ui/react'
+import { CloseIcon, HamburgerIcon } from '@chakra-ui/icons'
+
+function Menu() {
+    return (
+        <>
+            <Link>
+                Terbaru
+            </Link>
+            <Link>
+                Puisi
+            </Link>
+            <Link>
+                Tentang Saya
+            </Link>
+        </>
+    );
+}
 
 export default function Navigation() {
     const [isDesktop] = useMediaQuery('(min-width: 500px)')
+    const { isOpen, onClose, onOpen } = useDisclosure();
     return (
         <Flex p="2" height="36" alignItems="flex-start" bgGradient="linear(to-b, gray.900, gray.700)">
             <HStack align="flex-end" width="full" spacing="18">
@@ -12,25 +40,41 @@ export default function Navigation() {
                 {
                     isDesktop ? (
                         <HStack align="flex-end" spacing="6">
-                            <Link>
-                                Terbaru
-                            </Link>
-                            <Link>
-                                Puisi
-                            </Link>
-                            <Link>
-                                Tentang Saya
-                            </Link>
+                            <Menu />
                         </HStack>
                     ) : (
                         <>
                             <Spacer />
                             <IconButton
                                 variant="ghost"
+                                onClick={onOpen}
+                                display={isOpen ? 'none' : 'block'}
                                 icon={
                                     <HamburgerIcon />
                                 }
                             />
+                            <Box
+                                display={isOpen ? 'flex' : 'none'}
+                                position="absolute"
+                                top="0"
+                                left="0"
+                                width="100%"
+                                height="100vh"
+                                backgroundColor="gray.800"
+                                opacity="0.8"
+                            >
+                                <IconButton
+                                    pos="absolute"
+                                    top="3"
+                                    right="3"
+                                    onClick={onClose}
+                                >
+                                    <CloseIcon />
+                                </IconButton>
+                                <VStack alignItems="flex-start" padding="8" spacing="10">
+                                    <Menu />
+                                </VStack>
+                            </Box>
                         </>
                     )
                 }
